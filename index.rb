ddf00ce10a95e9ecd51ae1662e49c73f5f03da82
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-15 13:51:27 -1100

    Fix linters issues

--- index.rb
+++ index.rb
@@ -1,31 +1,11 @@
 # decode morse code character
 def decode_char(str)
   morse_code = {
-    '.-' => 'A',    
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
   }
   morse_code[str]
@@ -33,25 +13,25 @@ end
 
 # print decode_char('.-')
 
-#decode entire word
+# decode entire word
 def decode_word(str)
   word = ''
   # Split without parameters
-  # Here the pattern is a 
+  # Here the pattern is a
   # single whitespace
   items = str.split
-  items.each { |i| word+= decode_char(i) }
+  items.each { |i| word += decode_char(i) }
   word
 end
 
 # print decode_word('-- -.--')
 
-#decode message
+# decode message
 def decode(str)
   message = ''
   # Split by 3 spaces
   items = str.split('   ')
-  items.each { |i| message+= "#{decode_word(i)} " }
+  items.each { |i| message += "#{decode_word(i)} " }
   message
 end
 
